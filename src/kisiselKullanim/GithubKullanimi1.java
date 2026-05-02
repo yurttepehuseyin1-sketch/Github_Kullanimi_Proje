abca07cdd718ac8d979bea1b60d5babe023a0048
Hüseyin Yurttepe <yurttepehuseyin1@gmail.com>
2026-05-02 05:03:04 +0300

    ilk degisiklik

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -3,5 +3,7 @@ package kisiselKullanim;
 public class GithubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("That's it");
     }
 }
